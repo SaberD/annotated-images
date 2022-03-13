c241d997e1dddb9e1f5979bdd28430177af99052
Paul <paul2048@pm.me>
2022-03-13 15:03:18 +0000

    code consistency and readability; removed unused imports; grammar

--- annotated_images/split.py
+++ annotated_images/split.py
@@ -5,9 +5,7 @@ import pathlib
 import random
 import shutil
 import glob
-import json
-import xml.etree.cElementTree as et
-from os import path, listdir
+import os
 
 
 def list_dirs(directory):
@@ -20,21 +18,21 @@ def list_files(directory):
     """Returns files in a given directory
     Default only splits annotated images, if no annotations are found then all images are split.
     """
-    files = glob.glob(directory + "*.json")
+    files = glob.glob(directory + '*.json')
     if len(files) == 0:
-        files = glob.glob(directory + "*.xml")
+        files = glob.glob(directory + '*.xml')
     if len(files) == 0:
         files = glob.glob
     if len(files) == 0:
-        files = glob.glob(directory + "*.*")
+        files = glob.glob(directory + '*.*')
     return files
 
 
-def split(input_dir, output_dir="output", seed=1337, ratio=(.8, .1, .1)):
+def split(input_dir, output_dir='output', seed=1337, ratio=(.8, .1, .1)):
     """Copies files from input dir to folders in output dir
 
     Keeps the annotation data together with the images while splitting. 
-    To only split into train and test folder, only input ratio of len 2 (i.e. ratio=(0.8, 0.2)).
+    To only split into train and test folders, only input ratio of len 2 (i.e. ratio=(0.8, 0.2)).
 
     Args:
         input_dir (str): path to images and annotation data.
@@ -48,12 +46,13 @@ def split(input_dir, output_dir="output", seed=1337, ratio=(.8, .1, .1)):
     assert sum(ratio) == 1
     assert len(ratio) in (2, 3)
 
-    return  split_ratio(input_dir, output_dir, ratio, seed)
+    return split_ratio(input_dir, output_dir, ratio, seed)
+
 
 def setup_files(input_dir, seed):
     """Returns shuffled files
     """
-    # make sure its reproducible
+    # make sure it's reproducible
     random.seed(seed)
 
     files = list_files(input_dir)
@@ -61,6 +60,7 @@ def setup_files(input_dir, seed):
     files.sort()
     random.shuffle(files)
     return files
+
 
 def split_ratio(input_dir, output_dir, ratio, seed):
     """Splits one very class folder
@@ -75,7 +75,7 @@ def split_ratio(input_dir, output_dir, ratio, seed):
 
 
 def split_files(files, split_train, split_test, use_val):
-    """Splits the files along the provided indices
+    """Splits the files along with the provided indices
     Returns a dict with the number of images in each folder
     """
     files_train = files[:split_train]
@@ -97,16 +97,15 @@ def copy_files(files_type, output):
     out = dict()
     for (files, folder_type) in files_type:
         counter = 0
-        full_path = path.join(output, folder_type)
+        full_path = os.path.join(output, folder_type)
 
-        pathlib.Path(full_path).mkdir(
-            parents=True, exist_ok=True)
+        pathlib.Path(full_path).mkdir(parents=True, exist_ok=True)
         for f in files:
-            name = path.splitext(f)[0]
+            name = os.path.splitext(f)[0]
             counter += 1
-            for i in glob.glob(name+".*"):
+            for i in glob.glob(name + '.*'):
                 shutil.copy2(i, full_path)
         out[folder_type] = counter
         total += counter
-    out["total"]=total
+    out['total'] = total
     return out
